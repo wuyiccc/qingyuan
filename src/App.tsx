a53a60e69c01b2a5f93fa09834b16cf64d840a04
wuyiccc <3406324191@qq.com>
2024-01-22 21:57:55 +0800

    feat: Updating the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState } from 'react'
 
 function MyButton() {
   return <button>I'm a button</button>
@@ -61,6 +62,16 @@ function RespondingToEvent() {
   return <button onClick={handleClick}>Click me</button>
 }
 
+function UpdatingScreen() {
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setCount(count + 1)
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>
+}
+
 function App() {
   return (
     <div>
@@ -81,6 +92,7 @@ function App() {
       <ConditionalRendering></ConditionalRendering>
       <RenderList></RenderList>
       <RespondingToEvent></RespondingToEvent>
+      <UpdatingScreen></UpdatingScreen>
     </div>
   )
 }
