7b27ddfade4879dcd05d08284047675e4997f67f
wuyiccc <3406324191@qq.com>
2024-01-22 20:55:24 +0800

    feat: Writing markup with JSX

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,23 @@ function MyButton() {
   return <button>I'm a button</button>
 }
 
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Hello there. <br /> How do you do ?
+      </p>
+    </>
+  )
+}
+
 function App() {
   return (
     <div>
       <h1>welcome to my app</h1>
       <MyButton></MyButton>
+      <AboutPage></AboutPage>
     </div>
   )
 }
