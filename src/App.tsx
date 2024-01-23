9c839c3aabd9a937b2b2aa64f79eb61213701b6a
wuyiccc <3406324191@qq.com>
2024-01-23 22:59:24 +0800

    feat: memo persistence node element, useCallback persistence function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useMemo, useState } from 'react'
+import { memo, useCallback, useContext, useMemo, useState } from 'react'
 
 function UseMemoLearn() {
   const [count, setCount] = useState(0)
@@ -22,12 +22,28 @@ function UseMemoLearn() {
     return list.reduce((pre, cur) => pre + cur)
   }, [])
 
+  // use useCallback to persistence function, if dependency does not change, the function object does not recreate
+  const handleChildClick = useCallback(() => {
+    console.log('child node click')
+  }, [])
+
+  // memo use to persistence node, if callback not changed, this node does not rebuild
+  const Child = memo(({ callback }: any) => {
+    console.log('build Child node')
+    return (
+      <div>
+        <button onClick={callback}>child node</button>
+      </div>
+    )
+  })
+
   return (
     <div>
       <p>total1: {total1()}</p>
       <p>total2: {total2}</p>
       <p>{count}</p>
       <button onClick={handleSetCount}>add count</button>
+      <Child callback={handleChildClick}></Child>
     </div>
   )
 }
