f1a91abd946f47e54196cc0ac095c4e2e571f24d
wuyiccc <3406324191@qq.com>
2024-01-23 23:35:45 +0800

    feat: nested component data shared

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,34 @@
 import './App.css'
-import { memo, useCallback, useContext, useMemo, useState } from 'react'
+import React, { useContext } from 'react'
 
-function UseMemoLearn() {
-  const [count, setCount] = useState(0)
-
-  function handleSetCount() {
-    setCount(count => count + 1)
-  }
-
-  // when page refreshed the total1's function will execute again
-  const total1 = () => {
-    console.log('total1 execute')
-    const list = [1, 2, 3, 4, 5]
-    return list.reduce((pre, cur) => pre + cur)
-  }
-
-  // useMemo has memory function, if page refreshed, and dependency has not changed, the total2's function cannot execute again
-  const total2 = useMemo(() => {
-    console.log('total2 execute')
-    const list = [1, 2, 3, 4, 5]
-    return list.reduce((pre, cur) => pre + cur)
-  }, [])
-
-  // use useCallback to persistence function, if dependency does not change, the function object does not recreate
-  const handleChildClick = useCallback(() => {
-    console.log('child node click')
-  }, [])
-
-  // memo use to persistence node, if callback not changed, this node does not rebuild
-  const Child = memo(({ callback }: any) => {
-    console.log('build Child node')
-    return (
+const UserContext = React.createContext({ name: '' })
+function UserContextLearn() {
+  return (
+    <UserContext.Provider value={{ name: 'tom' }}>
       <div>
-        <button onClick={callback}>child node</button>
+        <Child1></Child1>
       </div>
-    )
-  })
+    </UserContext.Provider>
+  )
+}
+
+function Child1() {
+  return (
+    <div>
+      <p>
+        <span>Child1</span>
+      </p>
+      <Child2></Child2>
+    </div>
+  )
+}
+
+function Child2() {
+  const { name } = useContext(UserContext)
 
   return (
     <div>
-      <p>total1: {total1()}</p>
-      <p>total2: {total2}</p>
-      <p>{count}</p>
-      <button onClick={handleSetCount}>add count</button>
-      <Child callback={handleChildClick}></Child>
+      <p>Child2: {name}</p>
     </div>
   )
 }
@@ -52,7 +37,7 @@ function App() {
   return (
     <div className='App'>
       <p>欢迎学习React</p>
-      <UseMemoLearn></UseMemoLearn>
+      <UserContextLearn></UserContextLearn>
     </div>
   )
 }
