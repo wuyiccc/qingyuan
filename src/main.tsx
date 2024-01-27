ff8549f20871616cc66e3fee6829be2e70003e18
wuyiccc <3406324191@qq.com>
2024-01-27 20:11:55 +0800

    feat: react-router hash路由跳转

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { HashRouter, Route, Routes } from 'react-router-dom'
+
+function ReactDemo() {
+  return <h2>欢迎学习React课程</h2>
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter>
+    <Routes>
+      <Route path='/react' element={<App />}></Route>
+      <Route path='/demo' element={<ReactDemo />}></Route>
+    </Routes>
+  </HashRouter>
 )
