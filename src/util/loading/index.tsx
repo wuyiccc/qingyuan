94e8b8d80af1769fd5d77a7194f769d6badb0896
wuyiccc <3406324191@qq.com>
2024-01-28 22:27:18 +0800

    feat: 引入iconfont美化loading加载

--- src/util/loading/index.tsx
+++ src/util/loading/index.tsx
@@ -1,27 +1,25 @@
-import ReactDOM from 'react-dom/client'
-import Loading from '@/util/loading/loading.tsx'
+import './loading.less'
 
-let count = 0
+let requestCount = 0
 
 export const showLoading = () => {
-  if (count === 0) {
-    console.log('add loading')
-    const loading = document.createElement('div')
-    loading.setAttribute('id', 'loading')
-    document.body.appendChild(loading)
-    ReactDOM.createRoot(loading).render(<Loading />)
+  // 如果是第一个请求, 则显示loading组件
+  if (requestCount === 0) {
+    const loading = document.getElementById('loading')
+    loading?.style.setProperty('display', 'flex')
   }
-  count++
+  requestCount++
 }
 
 export const hideLoading = () => {
-  if (count < 0) {
+  if (requestCount < 0) {
     return
   }
-  count--
+  requestCount--
 
   // 最后一个请求结束之后才关闭loading
-  if (count === 0) {
-    document.body.removeChild(document.getElementById('loading') as HTMLDivElement)
+  if (requestCount === 0) {
+    const loading = document.getElementById('loading')
+    loading?.style.setProperty('display', 'none')
   }
 }
--- src/view/Welcome.tsx
+++ src/view/Welcome.tsx
@@ -1,3 +1,20 @@
+import request from '@/util/request.ts'
+import { Button } from 'antd'
+
+function handleClick() {
+  request.post('/user/login', {
+    username: 'admin',
+    password: '1234'
+  })
+}
+
 export default function Welcome() {
-  return <div className='welcome'>Welcome</div>
+  return (
+    <div className='welcome'>
+      <p>Welcome</p>
+      <p>
+        <Button onClick={handleClick}>点击事件</Button>
+      </p>
+    </div>
+  )
 }
