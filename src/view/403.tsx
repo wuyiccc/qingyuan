26254aec2470738761893236a32ef776ecadf700
wuyiccc <3406324191@qq.com>
2024-01-28 08:44:05 +0800

    feat: api路由定义

--- src/view/403.tsx
+++ src/view/403.tsx
@@ -1,3 +1,24 @@
-export default function Error403() {
-  return <div className='welcome'>403</div>
+import { useNavigate } from 'react-router-dom'
+import { Button, Result } from 'antd'
+
+function NotAuth() {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/')
+  }
+  return (
+    <Result
+      status={403}
+      title='403'
+      subTitle='抱歉, 您没有权限访问此页面'
+      extra={
+        <Button type='primary' onClick={handleClick}>
+          返回首页
+        </Button>
+      }
+    />
+  )
 }
+
+export default NotAuth
