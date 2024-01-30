8ec94b022537e7085890325aceb7fe80f66d045d
wuyiccc <3406324191@qq.com>
2024-01-30 22:33:23 +0800

    feat: 自定义主题色

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import { ConfigProvider } from 'antd'
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <ConfigProvider theme={{ token: { colorPrimary: '#282A36' } }}>
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  )
 }
 
 export default App
