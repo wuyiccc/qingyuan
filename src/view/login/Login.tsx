fe6f9984c27049f188eb57af9b31d0ec92f89f50
wuyiccc <3406324191@qq.com>
2024-01-30 22:20:30 +0800

    feat: css module

--- src/view/login/Login.tsx
+++ src/view/login/Login.tsx
@@ -2,14 +2,15 @@ import React, { useEffect } from 'react'
 import request from '@/util/request.ts'
 import './index.less'
 import { Button, Checkbox, Form, Input } from 'antd'
+import styles from './index.module.less'
 
 export default function Login() {
   const onFinish = () => {}
 
   return (
-    <div className='login'>
-      <div className='login-wrapper'>
-        <div className='title'>系统登录</div>
+    <div className={styles.login}>
+      <div className={styles.loginWrapper}>
+        <div className={styles.title}>系统登录</div>
         <Form name='basic' initialValues={{ remember: true }} onFinish={onFinish} autoComplete='off'>
           <Form.Item name='username' rules={[{ required: true, message: 'Please input your username!' }]}>
             <Input />
