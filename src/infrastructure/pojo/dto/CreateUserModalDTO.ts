4d5a772663ab6bab6bdd05a23254325a77916330
wuyiccc <3406324191@qq.com>
2024-02-07 23:11:41 +0800

    feat: 优化

--- src/infrastructure/pojo/dto/CreateUserModalDTO.ts
+++ src/infrastructure/pojo/dto/CreateUserModalDTO.ts
@@ -3,7 +3,7 @@ import { MutableRefObject } from 'react'
 
 class CreateUserModalDTO<T extends UserCreateBO> {
   // 内部定义open对象函数
-  mRef: MutableRefObject<{ open: (data?: T) => void } | undefined>
+  mRef: MutableRefObject<{ open: (data?: T) => void }>
   // 回调函数
   callback: () => void
 }
--- src/infrastructure/pojo/dto/UpdateUserModalDTO.ts
+++ src/infrastructure/pojo/dto/UpdateUserModalDTO.ts
@@ -2,7 +2,7 @@ import { MutableRefObject } from 'react'
 import UserEntity from '@/infrastructure/pojo/entity/UserEntity.ts'
 
 class UpdateUserModalDTO {
-  mRef: MutableRefObject<{ open: (data: UserEntity) => void } | undefined>
+  mRef: MutableRefObject<{ open: (data: UserEntity) => void }>
 
   callback: () => void
 }
