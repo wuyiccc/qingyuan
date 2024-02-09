bf1373bd3f428b839638ed857fddf8aede3119c5
wuyiccc <3406324191@qq.com>
2024-02-09 18:39:51 +0800

    feat: 引入molecule ide ui框架

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import './App.less'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
-import { ConfigProvider } from 'antd'
-import { App as AntdApp } from 'antd'
-import AntdGlobal from '@/component/message/AntdGlobal.tsx'
-import zhCN from 'antd/es/locale/zh_CN'
+import React from 'react'
+import { create, Workbench } from '@dtinsight/molecule'
+import '@dtinsight/molecule/esm/style/mo.css'
 
-function App() {
-  return (
-    <ConfigProvider locale={zhCN} theme={{ token: { colorPrimary: '#282A36' } }}>
-      <AntdApp>
-        <AntdGlobal />
-        <RouterProvider router={router} />
-      </AntdApp>
-    </ConfigProvider>
-  )
-}
+const moInstance = create({
+  extensions: []
+})
+
+const App = () => moInstance.render(<Workbench />)
 
 export default App
