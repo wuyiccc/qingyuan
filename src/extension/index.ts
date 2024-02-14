ee07b813bc7066f8f65adb75ecd83b09d9bcc2ad
wuyiccc <3406324191@qq.com>
2024-02-14 13:00:11 +0800

    feat: menuBar使用

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -2,7 +2,13 @@ import { IExtension } from '@dtinsight/molecule/esm/model'
 import { TheFirstExtension } from './TheFirstExtension'
 import { DataSourceExtension } from '@/extension/DataSource'
 import { TerminalExtension } from '@/extension/Terminal'
+import { MenuBarExtension } from '@/extension/menubar'
 
-const extensions: IExtension[] = [new TheFirstExtension(), new DataSourceExtension(), new TerminalExtension()]
+const extensions: IExtension[] = [
+  new TheFirstExtension(),
+  new DataSourceExtension(),
+  new TerminalExtension(),
+  new MenuBarExtension()
+]
 
 export default extensions
---
+++ src/extension/menubar/index.ts
@@ -0,0 +1,36 @@
+import molecule from '@dtinsight/molecule'
+import { IExtension, IMenuBarItem } from '@dtinsight/molecule/esm/model'
+import { IExtensionService } from '@dtinsight/molecule/esm/services'
+
+export const vscodeMenuItem: IMenuBarItem = {
+  id: 'menu.vscode',
+  name: 'wuyiccc',
+  icon: ''
+}
+
+export class MenuBarExtension implements IExtension {
+  id: string = 'MyMenubar'
+  name: string = 'MyMenu Bar'
+
+  activate(extensionCtx: IExtensionService): void {
+    this.initUI()
+  }
+
+  initUI() {
+    const { data } = molecule.menuBar.getState()
+    const nextData = data.concat()
+    nextData.push(vscodeMenuItem)
+
+    molecule.menuBar.setState({ data: nextData })
+    molecule.menuBar.onSelect(menuId => {
+      if (menuId === vscodeMenuItem.id) {
+        window.location.href = '#/'
+      }
+    })
+    molecule.layout.setMenuBarMode('horizontal')
+  }
+
+  dispose() {
+    molecule.menuBar.reset()
+  }
+}
