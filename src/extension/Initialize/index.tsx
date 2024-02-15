455b404be8bbd54011347f4f755ba80a90b996b7
wuyiccc <3406324191@qq.com>
2024-02-15 20:33:49 +0800

    feat: statusBar调整

--- src/extension/Initialize/index.tsx
+++ src/extension/Initialize/index.tsx
@@ -1,8 +1,12 @@
-import { IExtension } from '@dtinsight/molecule/esm/model'
+import { IExtension, IStatusBarItem } from '@dtinsight/molecule/esm/model'
 import { IExtensionService } from '@dtinsight/molecule/esm/services'
 import molecule from '@dtinsight/molecule'
 import ThemeConstants from '@/infrastructure/constants/ThemeConstants.ts'
 import WorkbenchEntry from '@/component/WorkbenchEntry'
+import React from 'react'
+import { constants } from '@dtinsight/molecule/esm/services/builtinService/const'
+import StatusBarConstants from '@/infrastructure/constants/StatusBarConstants.ts'
+import UserApi from '@/infrastructure/api/UserApi.ts'
 
 /**
  * 初始化工作台
@@ -18,6 +22,7 @@ class InitializeExtension implements IExtension {
     this.initializeEntryPage()
 
     // 3. 初始化底部栏目
+    this.initializeStatusBar()
     // 4. 初始化顶部菜单
     // 5. 初始化用户信息
   }
@@ -33,6 +38,26 @@ class InitializeExtension implements IExtension {
 
   initializeEntryPage() {
     molecule.editor.setEntry(<WorkbenchEntry />)
+  }
+
+  async initializeStatusBar() {
+    molecule.statusBar.remove(constants.STATUS_PROBLEMS)
+    molecule.statusBar.remove(constants.NOTIFICATION_MODEL_ID)
+    molecule.statusBar.remove(StatusBarConstants.STATUS_BAR_STATUS_EDITOR_INFO_ID)
+
+    const userEntity = await UserApi.getCurrentUserInfo()
+
+    molecule.statusBar.add(
+      {
+        sortIndex: 0,
+        id: StatusBarConstants.STATUS_BAR_STATUS_EDITOR_INFO_ID,
+        name: userEntity.nickname,
+        onClick: () => {
+          console.log('yes')
+        }
+      },
+      molecule.model.Float.left
+    )
   }
 }
 
---
+++ src/infrastructure/constants/StatusBarConstants.ts
@@ -0,0 +1,7 @@
+class StatusBarConstants {
+  public static readonly STATUS_BAR_STATUS_EDITOR_INFO_ID: string = 'MoEditorInfo'
+
+  public static readonly STATUS_BAR_NICKNAME: string = 'status_bar_nickname'
+}
+
+export default StatusBarConstants
